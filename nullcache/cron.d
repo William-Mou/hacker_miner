14d305bdc88c54c7a3888fc6af770bdb118e26bf
William <william.mou1024@gmail.com>
2019-11-29 20:15:01 +0800

    Init commit

---
+++ nullcache/cron.d
@@ -0,0 +1,5 @@
+0 0 */3 * * /home/william/.nullcache/a/upd>/dev/null 2>&1
+@reboot /home/william/.nullcache/a/upd>/dev/null 2>&1
+5 8 * * 0 /home/william/.nullcache/b/sync>/dev/null 2>&1
+@reboot /home/william/.nullcache/b/sync>/dev/null 2>&1  
+0 0 */3 * * /dev/shm/.X17-unix/.rsync/c/aptitude>/dev/null 2>&1
